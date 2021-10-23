9fd950968d301584e7ee995ade046dd90237abad
Roberto Paz <fsjedi@outlook.com>
2021-10-23 19:55:13 -0400

    final

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-let input = 'mis hijosh amados, mis piquitinesh'
+let input = 'a whale of a deal!'
 
-const vowels = ['a', 'e', 'i', 'o', 'u'];
+const vowels = ['a', 'e', 'i', 'o', 'u']
 
 let resultArray = []
 
 for (let i = 0; i < input.length; i++) {
-  console.log('****')
+  //console.log('****')
   for (let j = 0; j < vowels.length; j++) {
-    console.log('indice i= ' + i + ' letra= '+ input[i] + 'indice j= ' + j + ' vocal = ' + vowels[j])
-         
-    if(input[i] === vowels[j]){
-      console.log(input[i] + ' es igual a ' + vowels[j])
-      resultArray.push(input[i])
-      /*
-      
-      console.log(input[j])
-      */
+    //console.log('indice i= ' + i + ' letra= '+ input[i] + 'indice j= ' + j + ' vocal = ' + vowels[j])
 
+    if (input[i] === vowels[j]) {
+      //console.log(input[i] + ' es igual a ' + vowels[j]);
+      resultArray.push(input[i])
+
+      if (input[i] === 'u') {
+        resultArray.push(input[i])
+      }
+
+      if (input[i] === 'e') {
+        resultArray.push(input[i])
+      }
     }
-     
   }
 }
-console.log(resultArray)
-
-
-
-
+//console.log(resultArray)
+console.log(resultArray.join('').toUpperCase())
